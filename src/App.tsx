da1d5189b8fd9acb12e86a551350260004082c48
Igor E <igor@avans.no>
2021-03-21 15:32:59 +0100

    removed unnecessary noise

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,11 @@ import './App.css'
 import { useRootStore } from './root-store-context-provider'
 import { observer } from 'mobx-react-lite'
 import { Button } from 'antd'
-//import DocViewer from 'asma-doc-viewer'
-//import DocViewer from './modules/react-doc-viewer'
-//import DocViewer, { DocViewerRenderers} from 'react-doc-viewer'
 export const App = observer(function App() {
     const { count, onFieldChange } = useRootStore()
 
     return (
         <>
-            {/* <DocViewer documents={[{ uri: 'http://www.africau.edu/images/default/sample.pdf' }]} />  */}
-            {/* <DocViewer documents={[{ uri: 'http://www.africau.edu/images/default/sample.pdf' }]} /> */}
             <div className='App'>
                 <header className='App-header'>
                     <Logo className='App-logo' />
@@ -26,7 +21,6 @@ export const App = observer(function App() {
                     <p>
                         Edit <code>App.tsx</code> and save to test HMR updates.
                     </p>
-                    {/* <DocViewer documents={[]} /> */}
                     
                 </header>
             </div>
--- src/mst/Root.store.ts
+++ src/mst/Root.store.ts
@@ -3,6 +3,7 @@ import { types } from 'mobx-state-tree'
 export const Root$ = types
     .model('Root$', {
         count: 0,
+        some_aditional_field: 0,
     })
     .actions((self) => ({
         onFieldChange<Key extends keyof typeof self>(field: Key, value: typeof self[Key]) {
